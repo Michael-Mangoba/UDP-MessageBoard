a1c2934897efc6405ea3f7e4105680b3badebe8e
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2022-11-29 17:53:46 +0800

    Pseudo implemented /join
    Pseudo because cant seem to make client receive message from server so might need to make seperate classes for listening and sending

--- chatClient.java
+++ chatClient.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
   
 public class chatClient
 {
+    public static String Address;
+    public static Integer Port;
     public static void main(String args[]) throws IOException
     {
         Scanner sc = new Scanner(System.in);
@@ -14,29 +16,56 @@ public class chatClient
         // carrying the data.
         DatagramSocket ds = new DatagramSocket();
   
-        InetAddress ip = InetAddress.getLocalHost();
+        InetAddress ip = InetAddress.getByName(Address);
         byte buf[] = null;
-  
+        DatagramPacket DpReceive = null;
+
+        byte[] receive = new byte[65535];
         // loop while user not enters "bye"
         while (true)
         {
             String inp = sc.nextLine();
-  
+            String[] command = inp.split(" ");
+            if(command[0].equals("/join")){
+                connect(command);
+                buf = command[0].getBytes();
+                DatagramPacket DpSend =
+                  new DatagramPacket(buf, buf.length, ip, Port);
+                ds.send(DpSend);
+
+                DpReceive = new DatagramPacket(receive, receive.length);
+                ds.receive(DpReceive);
+                System.out.println("Server: " + data(receive));
+            }
             // convert the String input into the byte array.
             buf = inp.getBytes();
   
             // Step 2 : Create the datagramPacket for sending
             // the data.
-            DatagramPacket DpSend =
-                  new DatagramPacket(buf, buf.length, ip, 1234);
-  
-            // Step 3 : invoke the send call to actually send
-            // the data.
-            ds.send(DpSend);
+            ;
   
             // break the loop if user enters "bye"
             if (inp.equals("bye"))
                 break;
         }
+    }
+
+    private static void connect(String[] input){
+        Address = input[1];
+        Port = Integer.parseInt(input[2]);
+    }
+
+    public static StringBuilder data(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
     }
 }
--- chatServer.java
+++ chatServer.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
   
 public class chatServer
@@ -9,19 +10,25 @@ public class chatServer
     public static void main(String[] args) throws IOException
     {
         // Step 1 : Create a socket to listen at port 1234
+        InetAddress newIp = InetAddress.getLocalHost();
         DatagramSocket ds = new DatagramSocket(1234);
         byte[] receive = new byte[65535];
-  
+
+        byte buf[] = null;
+        System.out.println(newIp);
         DatagramPacket DpReceive = null;
         while (true)
         {
-  
-            // Step 2 : create a DatgramPacket to receive the data.
+          // Step 2 : create a DatgramPacket to receive the data.
             DpReceive = new DatagramPacket(receive, receive.length);
   
             // Step 3 : revieve the data in byte buffer.
             ds.receive(DpReceive);
-  
+            if(data(receive).equals("/join")){
+                buf = "Connection to the Message Board Server is successful!".getBytes();
+                DatagramPacket DpSend = new DatagramPacket(buf, buf.length, newIp, 1234);
+                ds.send(DpSend);
+            }
             System.out.println("Client:-" + data(receive));
   
             // Exit the server if the client sends "bye"
