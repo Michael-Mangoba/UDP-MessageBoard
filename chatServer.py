7694aaa452e9ef033e4bb9ff6ac8f27c3f5c2294
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2022-12-05 14:02:55 +0800

    Update chatServer.py

--- chatServer.py
+++ chatServer.py
@@ -29,9 +29,9 @@ def read():
             message = json.loads(data.decode())
             reply = {}
             content = ""
-            ip, port = addr.decode()
+            host, port = addr
             print("System Recieved: " + data.decode() + "\n")
-            print("Sender Info: " + ip + " " + port + "\n")
+            print("Sender Info: " + host + " " + port + "\n")
         
             if message["command"] == "join":
                 if addr not in clients:
